780e3380665e312f2eb3a788f41e5d6a4a479430
Mbaoma <mmaryraphaella@gmail.com>
2022-02-03 14:11:03 +0100

    final files

--- provider.tf
+++ provider.tf
@@ -10,16 +10,6 @@ terraform {
 }
 
 
-
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "2.59.0"
-#     }
-#   }
-# }
-
 provider "aws" {
   region = "eu-west-1"
 }
